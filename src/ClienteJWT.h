6db48604366fb6bd4a6a128047be327970ba25d5
Sofia C <42685693+SFC99@users.noreply.github.com>
2025-11-14 22:48:09 -0600

    Delete src directory

--- src/ClienteJWT.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-std::string loginServidor(const std::string& servidor, int puerto,
-                          const std::string& usuario, const std::string& password);
-
-std::string obtenerDatos(const std::string& servidor, int puerto, const std::string& token);
